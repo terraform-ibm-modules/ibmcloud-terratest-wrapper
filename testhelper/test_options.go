9ffae0df85f61917a21464294bc5eec50fe17e44
Todd Giguere <todd.giguere@ibm.com>
2022-10-17 11:25:22 -0400

    feat: added new option to testoptions to exclude AT regions

--- testhelper/test_options.go
+++ testhelper/test_options.go
@@ -16,28 +16,29 @@ const defaultRegionYaml = "../common-dev-assets/common-go-assets/cloudinfo-regio
 const ibmcloudApiKeyVar = "TF_VAR_ibmcloud_api_key"
 
 type TestOptions struct {
-	BestRegionYAMLPath      string                 // BestRegionYAMLPath Path to the yaml containing regions and weights
-	DefaultRegion           string                 // DefaultRegion default region if automatic detection fails
-	ResourceGroup           string                 // ResourceGroup IBM Cloud resource group to use
-	Region                  string                 // Region to use
-	TerraformVars           map[string]interface{} // TerraformVars variables to pass to terraform
-	TerraformDir            string                 // TerraformDir Directory Terraform files are in
-	TerraformOptions        *terraform.Options     `copier:"-"` // TerraformOptions Terraform options to use
-	UseTerraformWorkspace   bool                   // UseTerraformWorkspace Use a Terraform workspace
-	WorkspaceName           string                 // WorkspaceName name of the workspace
-	WorkspacePath           string                 // WorkspacePath path to workspace
-	RequiredEnvironmentVars map[string]string      // RequiredEnvironmentVars
-	Tags                    []string               // Tags optional tags to add
-	Prefix                  string                 // Prefix to use when creating resources
-	IgnoreAdds              Exemptions             // IgnoreAdds ignore adds (creates) to these resources in Consistency and Upgrade tests
-	IgnoreDestroys          Exemptions             // IgnoreDestroys ignore destroys to these resources in Consistency and Upgrade tests
-	IgnoreUpdates           Exemptions             // IgnoreUpdates ignore updates to these resources in Consistency and Upgrade tests
-	ImplicitDestroy         []string               // ImplicitDestroy Remove these resources from the State file to allow implicit destroy
-	ImplicitRequired        bool                   // ImplicitRequired If true the test will fail if the resource fails to be removed from the state file
-	Testing                 *testing.T             `copier:"-"` // Testing The current test object
-	IsUpgradeTest           bool                   // Identifies if current test is an UPGRADE test, used for special processing
-	UpgradeTestSkipped      bool                   // Informs the calling test that conditions were met to skip the upgrade test
-	baseTempWorkingDir      string                 // INTERNAL variable to store the base level of temporary working directory
+	BestRegionYAMLPath            string                 // BestRegionYAMLPath Path to the yaml containing regions and weights
+	DefaultRegion                 string                 // DefaultRegion default region if automatic detection fails
+	ResourceGroup                 string                 // ResourceGroup IBM Cloud resource group to use
+	Region                        string                 // Region to use
+	TerraformVars                 map[string]interface{} // TerraformVars variables to pass to terraform
+	TerraformDir                  string                 // TerraformDir Directory Terraform files are in
+	TerraformOptions              *terraform.Options     `copier:"-"` // TerraformOptions Terraform options to use
+	UseTerraformWorkspace         bool                   // UseTerraformWorkspace Use a Terraform workspace
+	WorkspaceName                 string                 // WorkspaceName name of the workspace
+	WorkspacePath                 string                 // WorkspacePath path to workspace
+	RequiredEnvironmentVars       map[string]string      // RequiredEnvironmentVars
+	Tags                          []string               // Tags optional tags to add
+	Prefix                        string                 // Prefix to use when creating resources
+	IgnoreAdds                    Exemptions             // IgnoreAdds ignore adds (creates) to these resources in Consistency and Upgrade tests
+	IgnoreDestroys                Exemptions             // IgnoreDestroys ignore destroys to these resources in Consistency and Upgrade tests
+	IgnoreUpdates                 Exemptions             // IgnoreUpdates ignore updates to these resources in Consistency and Upgrade tests
+	ImplicitDestroy               []string               // ImplicitDestroy Remove these resources from the State file to allow implicit destroy
+	ImplicitRequired              bool                   // ImplicitRequired If true the test will fail if the resource fails to be removed from the state file
+	Testing                       *testing.T             `copier:"-"` // Testing The current test object
+	IsUpgradeTest                 bool                   // Identifies if current test is an UPGRADE test, used for special processing
+	UpgradeTestSkipped            bool                   // Informs the calling test that conditions were met to skip the upgrade test
+	baseTempWorkingDir            string                 // INTERNAL variable to store the base level of temporary working directory
+	ExcludeActivityTrackerRegions bool                   // Will exclude any VPC regions that already contain an Activity Tracker
 }
 
 func TestOptionsDefaultWithVars(originalOptions *TestOptions) *TestOptions {
@@ -80,10 +81,11 @@ func TestOptionsDefault(originalOptions *TestOptions) *TestOptions {
 		// Get the best region
 		// Programmatically determine region to use based on availability
 		// Set OS environment variable FORCE_TEST_REGION to force a specific region
+		regionOptions := &TesthelperTerraformOptions{ExcludeActivityTrackerRegions: newOptions.ExcludeActivityTrackerRegions}
 		if newOptions.BestRegionYAMLPath != "" {
-			newOptions.Region, _ = GetBestVpcRegion(newOptions.RequiredEnvironmentVars[ibmcloudApiKeyVar], newOptions.BestRegionYAMLPath, newOptions.DefaultRegion)
+			newOptions.Region, _ = GetBestVpcRegionO(newOptions.RequiredEnvironmentVars[ibmcloudApiKeyVar], newOptions.BestRegionYAMLPath, newOptions.DefaultRegion, *regionOptions)
 		} else {
-			newOptions.Region, _ = GetBestVpcRegion(newOptions.RequiredEnvironmentVars[ibmcloudApiKeyVar], defaultRegionYaml, newOptions.DefaultRegion)
+			newOptions.Region, _ = GetBestVpcRegionO(newOptions.RequiredEnvironmentVars[ibmcloudApiKeyVar], defaultRegionYaml, newOptions.DefaultRegion, *regionOptions)
 		}
 	}
 
