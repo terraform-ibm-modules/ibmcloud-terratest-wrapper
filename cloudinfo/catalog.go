c9357dd54b4f53696a7eff497a4b51de2b3f970b
Daniel Butler <dannyeb@gmail.com>
2024-05-16 14:51:15 +0100

    add catalog functions

---
+++ cloudinfo/catalog.go
@@ -0,0 +1,32 @@
+package cloudinfo
+
+import (
+	"fmt"
+	"github.com/IBM/platform-services-go-sdk/catalogmanagementv1"
+)
+
+func (infoSvc *CloudInfoService) GetVersion(versionLocator string) (*catalogmanagementv1.Version, error) {
+	// Call the GetVersion method with the version locator and the context
+	getVersionOptions := &catalogmanagementv1.GetVersionOptions{
+		VersionLocID: &versionLocator,
+	}
+
+	offering, response, err := infoSvc.catalogService.GetVersion(getVersionOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if the response status code is 200 (success)
+	if response.StatusCode == 200 {
+		var version catalogmanagementv1.Version
+		if offering.Kinds != nil {
+			version = offering.Kinds[0].Versions[0]
+		}
+		if &version != nil {
+			return &version, nil
+		}
+		return nil, fmt.Errorf("version not found")
+	}
+
+	return nil, fmt.Errorf("failed to get version: %s", response.RawResult)
+}
