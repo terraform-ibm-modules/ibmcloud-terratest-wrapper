cb2f18faedaf440ddfbbc376f13ba0ef03b381e4
Ankit Kumar <ankit@Ankits-MacBook-Pro.local>
2026-01-28 17:06:37 +0000

    test: updated GetRandomAdminPassword and added testfor the function

--- common/random.go
+++ common/random.go
@@ -1,16 +1,12 @@
 package common
 
 import (
-	"bytes"
 	"crypto/rand"
-	"encoding/base64"
 	"math/big"
-	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 const base36chars = "0123456789abcdefghijklmnopqrstuvwxyz"
+const base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
 
 // UniqueId returns a unique (ish) id we can attach to prefix variable passed in terraform configuration
 // length of the random string to be appended can be controlled by length argument passed to the function
@@ -23,31 +19,66 @@ func UniqueId(length ...int) string {
 	if len(length) > 0 && length[0] > 0 {
 		idLength = length[0]
 	}
-	var out bytes.Buffer
 
-	for i := 0; i < idLength; i++ {
-		out.WriteByte(base36chars[CryptoIntn(len(base36chars))])
-	}
-
-	return out.String()
+	return RandomStringGenerator(idLength, base36chars)
 }
 
+// GetRandomPasswordWithPrefix generates a random password string that always starts
+// with a required prefix.
+//
+// If a prefix is provided, it is used as the prefix; otherwise the default
+// prefix "A1a" is used (handy for satisfying common complexity rules like upper/lower/digit).
+//
+// The returned password is the prefix plus 12 additional cryptographically secure
+// random characters chosen from base64chars (via RandomStringGenerator).
+//
+// Note: the total length of the returned string is len(prefix) + 12.
+func GetRandomPasswordWithPrefix(prefix ...string) string {
+
+	// Default prefix to satisfy common complexity rules
+	defaultPrefix := "A1a"
+
+	// Override default prefix if provided
+	if len(prefix) > 0 && prefix[0] != "" {
+		defaultPrefix = prefix[0]
+	}
+
+	length := 12
+	randomPass := defaultPrefix + RandomStringGenerator(length, base64chars)
+
+	return randomPass
+}
+
+// RandomStringGenerator returns a cryptographically-secure random string of the given
+// length, where each character is sampled uniformly at random from characterSet.
+//
+// If length <= 0 or characterSet is empty, it returns an empty string.
+//
+// Randomness is sourced from CryptoIntn, which uses crypto/rand,
+// making it suitable for passwords, tokens, and other security-sensitive values.
+func RandomStringGenerator(length int, characterSet string) string {
+	if length <= 0 || len(characterSet) == 0 {
+		return ""
+	}
+
+	b := make([]byte, length)
+	for i := 0; i < length; i++ {
+		b[i] = characterSet[CryptoIntn(len(characterSet))]
+	}
+	return string(b)
+}
+
+// CryptoIntn returns a cryptographically-secure random integer n in the range [0, max).
+//
+// It uses crypto/rand.Int with a uniform distribution over the specified bound.
+// This function panics if the underlying random read fails.
+//
+// Callers must ensure max > 0; passing max <= 0 will cause big.NewInt to panic or
+// crypto/rand.Int to error.
 func CryptoIntn(max int) int {
 	n, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
 	if err != nil {
 		panic(err)
 	}
 	return int(n.Int64())
-}
-
-// GetRandomAdminPassword generates a random admin password for use in tests
-func GetRandomAdminPassword(t *testing.T) string {
-	// Generate a 15 char long random string for the admin_pass
-	randomBytes := make([]byte, 13)
-	_, randErr := rand.Read(randomBytes)
-	require.Nil(t, randErr) // do not proceed if we can't gen a random password
-
-	randomPass := "A1a" + base64.URLEncoding.EncodeToString(randomBytes)[:12]
-
-	return randomPass
 }
