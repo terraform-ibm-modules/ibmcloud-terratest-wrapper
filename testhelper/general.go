89e00288204b2020684fffc634a7576fc6694c49
Harika Ponna <harika.ponna@ibm.com>
2026-04-20 12:42:53 +0530

    test: add test comment for go.work testing

--- testhelper/general.go
+++ testhelper/general.go
@@ -14,6 +14,7 @@ import (
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
 )
+// TEST COMMENT: Testing go.work documentation approach
 
 const ForceTestRegionEnvName = "FORCE_TEST_REGION"
 
